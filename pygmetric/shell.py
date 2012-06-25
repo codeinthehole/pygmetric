d4c421d339b1176ca6a30a0f84067dd5223fda4a
David Winterbottom <david.winterbottom@gmail.com>
2012-06-25 11:11:43 +0100

    Found issue with units passing - ignoring it for now

--- pygmetric/shell.py
+++ pygmetric/shell.py
@@ -17,7 +17,7 @@ def call_gmetric(name, value, type='float', units="", debug=False):
     """
     Submit a metric to the gmond daemon
     """
-    cmd = 'gmetric --type=%s --name=%s --value=%s --units="%s"' % (type, name, value, units)
+    cmd = 'gmetric --type=%s --name=%s --value=%s' % (type, name, value)
     if debug:
         print cmd
     else:
